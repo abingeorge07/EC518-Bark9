6609d4e2592daa8eaa452d6089c973100e8ee70a
Abin George <abin_george07@outlook.com>
2024-11-19 15:39:51 -0500

    camera works for google robto too

--- model_test.py
+++ model_test.py
@@ -23,6 +23,7 @@ def windowView(model, data, opt, camera, scene, context, window):
 
 # Paths to models
 patriq = "PATRIQ/patriq.xml"
+example_quad = "models/google_barkour_v0/scene_mjx.xml"
 
 # Load the model and data
 model = mujoco.MjModel.from_xml_path(patriq)
@@ -35,12 +36,17 @@ opt = mujoco.MjvOption()
 # Set the camera for the renderer
 camera_name = "main_camera"
 camera_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_CAMERA, camera_name)
-
-# Set the camera for the viewer (onboard)
-camera = mujoco.MjvCamera()
-camera.type = mujoco.mjtCamera.mjCAMERA_FIXED
-camera.fixedcamid = camera_id
-
+    
+if(camera_id < 0):
+    # Set the camera for the viewer (onboard)
+    camera = mujoco.MjvCamera()
+    pov_working = False
+else:
+    # Set the camera for the viewer (onboard)
+    camera = mujoco.MjvCamera()
+    camera.type = mujoco.mjtCamera.mjCAMERA_FIXED
+    camera.fixedcamid = camera_id
+    pov_working = True
 
 # Top View Camera
 camera_top = mujoco.MjvCamera()
@@ -50,14 +56,16 @@ camera_top = mujoco.MjvCamera()
 # Initialize the viewer
 mujoco.glfw.glfw.init()
 
-# Create a window
-window = mujoco.glfw.glfw.create_window(1200, 900, "POV", None, None)
-# make the context current
-mujoco.glfw.glfw.make_context_current(window)
-mujoco.glfw.glfw.swap_interval(1)
-# Create a scene and context
-scene = mujoco.MjvScene(model, maxgeom=10000)
-context = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_150.value)
+
+if(pov_working):
+    # Create a window
+    window = mujoco.glfw.glfw.create_window(1200, 900, "POV", None, None)
+    # make the context current
+    mujoco.glfw.glfw.make_context_current(window)
+    mujoco.glfw.glfw.swap_interval(1)
+    # Create a scene and context
+    scene = mujoco.MjvScene(model, maxgeom=10000)
+    context = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_150.value)
 
 
 window_top = mujoco.glfw.glfw.create_window(1200, 900, "Top-Down", None, None)
@@ -68,7 +76,7 @@ scene_top = mujoco.MjvScene(model, maxgeom=10000)
 context_top = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_150.value)
 
 
-while not mujoco.glfw.glfw.window_should_close(window):
+while not mujoco.glfw.glfw.window_should_close(window_top):
     simstart = data.time
 
     # data.ctrl[0] = 0.1
@@ -79,7 +87,8 @@ while not mujoco.glfw.glfw.window_should_close(window):
         mujoco.mj_step(model, data)
 
     ## WINDOW 1 ##
-    windowView(model, data, opt, camera, scene, context, window)
+    if pov_working:
+        windowView(model, data, opt, camera, scene, context, window)
   
     ## WINDOW 2 ##
     windowView(model, data, opt, camera_top, scene_top, context_top, window_top)
