cc321a3d1fe0c665e6bcb3abd4d397afdd159855
Abin George <abin_george07@outlook.com>
2024-12-07 12:37:39 -0500

    few updates

--- model_test.py
+++ model_test.py
@@ -2,11 +2,15 @@ import mujoco
 import mujoco_viewer
 import cv2
 import numpy as np
+from heightMap import *
+from constant import *
+import time
+
 
 # function to render the window (pov)
 def windowView(model, data, opt, camera, scene, context, window):
     # Render the scene
-    viewport = mujoco.MjrRect(0, 0, 1200, 900)
+    viewport = mujoco.MjrRect(0, 0, WIDTH, HEIGHT)
 
     # make the current window context 
     mujoco.glfw.glfw.make_context_current(window)
@@ -59,7 +63,7 @@ mujoco.glfw.glfw.init()
 
 if(pov_working):
     # Create a window
-    window = mujoco.glfw.glfw.create_window(1200, 900, "POV", None, None)
+    window = mujoco.glfw.glfw.create_window(WIDTH, HEIGHT, "POV", None, None)
     # make the context current
     mujoco.glfw.glfw.make_context_current(window)
     mujoco.glfw.glfw.swap_interval(1)
@@ -68,7 +72,7 @@ if(pov_working):
     context = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_150.value)
 
 
-window_top = mujoco.glfw.glfw.create_window(1200, 900, "Top-Down", None, None)
+window_top = mujoco.glfw.glfw.create_window(WIDTH, HEIGHT, "Top-Down", None, None)
 mujoco.glfw.glfw.make_context_current(window_top)
 mujoco.glfw.glfw.swap_interval(1)
 
@@ -80,18 +84,34 @@ while not mujoco.glfw.glfw.window_should_close(window_top):
     simstart = data.time
 
     # data.ctrl[0] = 0.1
-    data.ctrl[1] = 0.1
-    data.ctrl[2] = 0.1
+    data.ctrl[1] = 0.001
+    data.ctrl[2] = 0.001
 
     while (data.time - simstart < 1.0/60.0):
         mujoco.mj_step(model, data)
 
-    ## WINDOW 1 ##
+    # ## WINDOW 1 ##
     if pov_working:
         windowView(model, data, opt, camera, scene, context, window)
-  
+        
+        # Get the height map 
+        depth_buffer, rgb_buffer = get_height_map(model, data, camera, scene, context, window)
+
+        # Finding teh height map from the rgb buffer
+        # depth_buffer = get_height_map_from_rgb(rgb_buffer)
+
+        # Display the height map
+        cv2.imshow("Height Map", depth_buffer)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+    
     ## WINDOW 2 ##
     windowView(model, data, opt, camera_top, scene_top, context_top, window_top)
+    time.sleep(0.1)
+
 
 
 
