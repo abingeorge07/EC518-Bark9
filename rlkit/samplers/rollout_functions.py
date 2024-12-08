34eebda66257bb2b581f45ebd3a3b75866eb74ab
Abin George <abin_george07@outlook.com>
2024-12-08 00:29:56 -0500

    working SAC

--- rlkit/samplers/rollout_functions.py
+++ rlkit/samplers/rollout_functions.py
@@ -106,6 +106,7 @@ def rollout(
     while path_length < max_path_length:
         raw_obs.append(o)
         o_for_agent = preprocess_obs_for_policy_fn(o)
+        
         a, agent_info = agent.get_action(o_for_agent, **get_action_kwargs)
 
         if full_o_postprocess_func:
